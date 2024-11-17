ddb7106ca302a49aaf36c9b6a4811b57e30ad92b
Joseph <josephmateop23@gmail.com>
2024-11-17 17:02:13 -0500

    404.css

--- unauthorized.php
+++ unauthorized.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Acceso Denegado</title>
+    <link href="/haras/css/styles.css" rel="stylesheet" />
     <style>
         /* Estilo general de la página */
         body {
