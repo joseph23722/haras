e4f98a559d1e7a3143203e0feee42f6edf7e906f
Francescoli <vicharramarcosgereda@gmail.com>
2024-11-23 01:08:13 -0500

    depurando I. caballos

--- views/inventarios/registrar-implementos-caballos.php
+++ views/inventarios/registrar-implementos-caballos.php
@@ -262,15 +262,4 @@
 
 <script src="/haras/vendor/alimentos/historial-alimentos.js" defer></script>
 <script src="/haras/vendor/alimentos/listar-alimentos.js" defer></script>
-
-
-<script src="https://cdn.datatables.net/buttons/2.0.1/js/dataTables.buttons.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/2.0.1/js/buttons.html5.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/2.0.1/js/buttons.print.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
-
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-<script src="../../swalcustom.js"></script>
 <script src="../../JS/implementoEquino.js"></script>
