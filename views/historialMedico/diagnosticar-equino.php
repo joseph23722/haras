40057f821454d3ec62548d77dff42d73a6a0d9a3
Francescoli <vicharramarcosgereda@gmail.com>
2024-11-26 23:51:50 -0500

    modal diagnostico avanzado

--- views/historialMedico/diagnosticar-equino.php
+++ views/historialMedico/diagnosticar-equino.php
@@ -250,5 +250,7 @@
 </div>
 
 <?php require_once '../footer.php'; ?>
-
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
 <script src="../../JS/historialmedico.js"></script>
