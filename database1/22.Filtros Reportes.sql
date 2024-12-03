7e589854075ff7c9a50b05df8dfbbd0fefd054a0
Joseph <josephmateop23@gmail.com>
2024-12-02 19:34:07 -0500

    filtros alimentos por stock

--- database1/22.Filtros Reportes.sql
+++ database1/22.Filtros Reportes.sql
@@ -2,46 +2,45 @@
 DROP PROCEDURE IF EXISTS `spu_filtrarAlimentos`;
 DELIMITER $$
 CREATE PROCEDURE spu_filtrarAlimentos(
-    IN _fechaCaducidadInicio DATE,
-    IN _fechaCaducidadFin DATE,
-    IN _fechaRegistroInicio DATETIME,
-    IN _fechaRegistroFin DATETIME
+    IN _orden VARCHAR(10) -- 'ASC' para ascendente, 'DESC' para descendente
 )
 BEGIN
-    -- Realizar la consulta de los alimentos con los filtros especificados
-    SELECT 
-        A.idAlimento,
-        A.idUsuario,
-        A.nombreAlimento,
-        TA.tipoAlimento AS nombreTipoAlimento,
-        A.stockActual,
-        A.stockMinimo,
-        A.estado,
-        U.nombreUnidad AS unidadMedidaNombre,
-        A.costo,
-        A.idLote,
-        A.idEquino,
-        A.compra,
-        A.fechaMovimiento,
-        L.idLote AS loteId,
-        L.lote,
-        L.fechaCaducidad,
-        L.fechaIngreso,
-        L.estadoLote
-    FROM 
-        Alimentos A
-    INNER JOIN 
-        LotesAlimento L ON A.idLote = L.idLote
-    INNER JOIN 
-        TipoAlimentos TA ON A.idTipoAlimento = TA.idTipoAlimento
-    INNER JOIN 
-        UnidadesMedidaAlimento U ON A.idUnidadMedida = U.idUnidadMedida
-    WHERE 
-        (_fechaCaducidadInicio IS NULL OR L.fechaCaducidad >= _fechaCaducidadInicio)
-        AND (_fechaCaducidadFin IS NULL OR L.fechaCaducidad <= _fechaCaducidadFin)
-        AND (_fechaRegistroInicio IS NULL OR L.fechaIngreso >= _fechaRegistroInicio)
-        AND (_fechaRegistroFin IS NULL OR L.fechaIngreso <= _fechaRegistroFin);
+    -- Declarar la variable para la consulta dinámica
+    SET @sql = CONCAT('
+        SELECT 
+            A.idAlimento,
+            A.idUsuario,
+            A.nombreAlimento,
+            TA.tipoAlimento AS nombreTipoAlimento,
+            A.stockActual,
+            A.stockMinimo,
+            A.estado,
+            U.nombreUnidad AS unidadMedidaNombre,
+            A.costo,
+            A.idLote,
+            A.idEquino,
+            A.compra,
+            A.fechaMovimiento,
+            L.idLote AS loteId,
+            L.lote,
+            L.fechaCaducidad,
+            L.fechaIngreso,
+            L.estadoLote
+        FROM 
+            Alimentos A
+        INNER JOIN 
+            LotesAlimento L ON A.idLote = L.idLote
+        INNER JOIN 
+            TipoAlimentos TA ON A.idTipoAlimento = TA.idTipoAlimento
+        INNER JOIN 
+            UnidadesMedidaAlimento U ON A.idUnidadMedida = U.idUnidadMedida
+        ORDER BY 
+            A.stockActual ', _orden);
 
+    -- Preparar y ejecutar la consulta dinámica
+    PREPARE stmt FROM @sql;
+    EXECUTE stmt;
+    DEALLOCATE PREPARE stmt;
 END $$
 DELIMITER ;
 
