c5ddeeaec5e8477b9d0aba1fc615e27cf8cd060f
Joseph <josephmateop23@gmail.com>
2024-12-02 21:14:35 -0500

    arreglando busqueda

--- views/inventarios/administrar-alimento.php
+++ views/inventarios/administrar-alimento.php
@@ -393,7 +393,6 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.1/jspdf.plugin.autotable.min.js"></script>
 
 
-
 <script src="/haras/vendor/alimentos/historial-alimentos.js" defer></script>
 <script src="/haras/vendor/alimentos/listar-alimentos.js" defer></script>
 <script id="jqueryScript" src="https://code.jquery.com/jquery-3.6.4.min.js" defer></script>
