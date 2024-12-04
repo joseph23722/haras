7865918ef99b09363a0baf16fae8e7dc96f4326d
Joseph <josephmateop23@gmail.com>
2024-12-04 17:22:10 -0500

    eliminando espacios

--- views/inventarios/administrar-medicamento.php
+++ views/inventarios/administrar-medicamento.php
@@ -360,9 +360,6 @@
         </div>
     </div>
 
-
-
-
     <!-- Modal para listar sugerencias -->
     <div class="modal fade" id="modalSugerencias" tabindex="-1" aria-labelledby="modalSugerenciasLabel" aria-hidden="true">
         <div class="modal-dialog modal-xl"> <!-- modal-xl para ancho extra -->
