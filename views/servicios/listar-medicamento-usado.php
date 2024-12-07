404f0a98f9e20749c109a6ac05a5c01d43baeffc
Francescoli <vicharramarcosgereda@gmail.com>
2024-12-07 17:14:56 -0600

    confusion de columnas

--- views/servicios/listar-medicamento-usado.php
+++ views/servicios/listar-medicamento-usado.php
@@ -1,7 +1,7 @@
 <?php require_once '../header.php'; ?>
 
 <div class="container-fluid px-4">
-    <h1 class="mt-4 text-center text-uppercase" style="font-weight: bold; font-size: 32px; color: #005b99;">Registro de Servicio Mixto</h1>
+    <h1 class="mt-4 text-center text-uppercase" style="font-weight: bold; font-size: 32px; color: #005b99;">Medicamentos Aplicados</h1>
     <ol class="breadcrumb mb-4 p-2 rounded" style="background-color: #e8f1f8;">
         <li class="breadcrumb-item active" style="color: #004085; font-weight: bold;">
             <i class="fas fa-info-circle" style="color: #007bff;"></i> Listado de medicamentos aplicados
