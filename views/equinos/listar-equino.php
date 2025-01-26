dcc5d8eb27a501ff9fa37d8eaf1abc3cb0c25640
Francescoli <vicharramarcosgereda@gmail.com>
2025-01-25 21:10:28 -0500

    vista listado

--- views/equinos/listar-equino.php
+++ views/equinos/listar-equino.php
@@ -2,18 +2,16 @@
 
 <div class="container-fluid px-4">
     <!-- Título principal -->
-    <h1 class="mt-4 text-center text-uppercase" style="font-weight: bold; font-size: 36px; color: #005b99;">
-        <i class="fas fa-horse-head" style="color: #a0ffb8;"></i> Listado de Equinos
+    <h1 class="mt-4 text-center text-uppercase" style="font-weight: bold; font-size: 36px; color: #000;">
+        <i class="fas fa-horse-head" style="color: #000;"></i> Listado de Equinos
     </h1>
 
     <!-- Sección de Listado de Equinos -->
     <div class="row">
         <div class="col-md-12">
             <div class="card shadow border-0 mt-4">
-                <div class="card-header" style="background: linear-gradient(to right, #a0ffb8, #9be8e4); color: #003366;">
-                    <h5 class="text-center m-0" style="font-weight: bold;">
-                        <i class="fas fa-database" style="color: #3498db;"></i> Datos de los Equinos
-                    </h5>
+                <div class="card-header" style="background: linear-gradient(to left, #123524, #356C56); color: #EFE3C2;">
+                    <h5 class="text-center m-0" style="font-weight: bold;"></h5>
                 </div>
                 <div class="card-body p-4" style="background-color: #f9f9f9;">
                     <table id="tabla-equinos" class="table table-striped table-hover table-bordered">
