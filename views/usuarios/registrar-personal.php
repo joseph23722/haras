222ef4ffcf3b8c5a278b0a76f47ab32277ef162f
Francescoli <vicharramarcosgereda@gmail.com>
2025-01-27 17:45:24 -0500

    vista usuarios

--- views/usuarios/registrar-personal.php
+++ views/usuarios/registrar-personal.php
@@ -2,8 +2,8 @@
 
 <main>
   <div class="container-fluid px-4">
-    <h1 class="mt-4 text-center text-uppercase" style="font-weight: bold; font-size: 36px; color: #005b99;">
-      <i class="fas fa-users" style="color: #a0ffb8;"></i> Registrar Personal y Listado de Usuarios
+    <h1 class="mt-4 text-center text-uppercase" style="font-weight: bold; font-size: 36px; color: #000;">
+      <i class="fas fa-users" style="color: #000;"></i> Registrar Personal y Listado de Usuarios
     </h1>
 
     <!-- Sección de Registro de Personal -->
@@ -11,10 +11,7 @@
       <div class="col-md-12 mb-4">
         <form action="" autocomplete="off" id="formulario-personal">
           <div class="card shadow border-0">
-            <div class="card-header" style="background: linear-gradient(to right, #a0ffb8, #9be8e4); color: #003366;">
-              <h5 class="text-center m-0" style="font-weight: bold;">
-                <i class="fas fa-id-card-alt" style="color: #3498db;"></i> Datos del Personal
-              </h5>
+            <div class="card-header" style="background: linear-gradient(to left, #123524, #356C56);">
             </div>
             <div class="card-body" style="background-color: #f9f9f9;">
               <!-- Fila 1 -->
@@ -22,19 +19,19 @@
                 <div class="col-md-2">
                   <div class="form-floating">
                     <input type="text" class="form-control" id="dni" pattern="[0-9]+" title="Solo se permiten números" maxlength="8" required autofocus>
-                    <label for="dni"><i class="fas fa-id-badge" style="color: #3498db;"></i> Nro. Documento</label>
+                    <label for="dni"><i class="fas fa-id-badge" style="color: #001F3F;"></i> Nro. Documento</label>
                   </div>
                 </div>
                 <div class="col-md-5">
                   <div class="form-floating">
                     <input type="text" class="form-control" id="apellidos" maxlength="100" required>
-                    <label for="apellidos"><i class="fas fa-user" style="color: #3498db;"></i> Apellidos Completos</label>
+                    <label for="apellidos"><i class="fas fa-user" style="color: #001F3F;"></i> Apellidos Completos</label>
                   </div>
                 </div>
                 <div class="col-md-5">
                   <div class="form-floating">
                     <input type="text" class="form-control" id="nombres" maxlength="100" required>
-                    <label for="nombres"><i class="fas fa-user-tag" style="color: #3498db;"></i> Nombres Completos</label>
+                    <label for="nombres"><i class="fas fa-user-tag" style="color: #001F3F;"></i> Nombres Completos</label>
                   </div>
                 </div>
               </div>
@@ -50,19 +47,19 @@
                       <option value="Pasaporte">Pasaporte</option>
                       <option value="Carnet">Carnet</option>
                     </select>
-                    <label for="tipodoc"><i class="fas fa-passport" style="color: #3498db;"></i> Tipo de documento</label>
+                    <label for="tipodoc"><i class="fas fa-passport" style="color: #001F3F;"></i> Tipo de documento</label>
                   </div>
                 </div>
                 <div class="col-md-3">
                   <div class="form-floating">
                     <input type="text" class="form-control" id="direccion" maxlength="255">
-                    <label for="direccion"><i class="fas fa-map-marker-alt" style="color: #3498db;"></i> Dirección</label>
+                    <label for="direccion"><i class="fas fa-map-marker-alt" style="color: #001F3F;"></i> Dirección</label>
                   </div>
                 </div>
                 <div class="col-md-3">
                   <div class="form-floating">
                     <input type="date" class="form-control" id="fechaIngreso">
-                    <label for="fechaIngreso"><i class="fas fa-calendar-check" style="color: #3498db;"></i> Fecha de Ingreso</label>
+                    <label for="fechaIngreso"><i class="fas fa-calendar-check" style="color: #001F3F;"></i> Fecha de Ingreso</label>
                   </div>
                 </div>
                 <div class="col-md-3">
@@ -74,17 +71,17 @@
                       <option value="PorPracticas">Por Prácticas</option>
                       <option value="Otros">Otros</option>
                     </select>
-                    <label for="tipoContrato"><i class="fas fa-briefcase" style="color: #3498db;"></i> Tipo de contrato</label>
+                    <label for="tipoContrato"><i class="fas fa-briefcase" style="color: #001F3F;"></i> Tipo de contrato</label>
                   </div>
                 </div>
               </div>
               <!-- Fin Fila 2 -->
             </div>
             <div class="card-footer text-end">
-              <button type="submit" class="btn btn-primary btn-lg shadow-sm me-2" style="background-color: #0056b3;">
+              <button type="submit" class="btn btn-lg shadow-sm me-2" style="background-color: #123524; color: #EFE3C2">
                 <i class="fas fa-save"></i> Registrar
               </button>
-              <button type="reset" class="btn btn-secondary btn-lg shadow-sm" style="background-color: #adb5bd;">
+              <button type="reset" class="btn btn-lg shadow-sm" style="background-color: #adb5bd;">
                 <i class="fas fa-times"></i> Cancelar
               </button>
             </div>
@@ -95,24 +92,24 @@
       <!-- Sección de Listado de Usuarios -->
       <div class="col-md-12">
         <div class="card shadow border-0">
-          <div class="card-header" style="background: linear-gradient(to right, #a0ffb8, #9be8e4); color: #003366;">
+          <div class="card-header" style="background: linear-gradient(to left, #123524, #356C56); color: #EFE3C2;">
             <h5 class="text-center m-0" style="font-weight: bold;">
-              <i class="fas fa-users" style="color: #3498db;"></i> Datos de los Usuarios
+              <i class="fas fa-users" style="color: #EFE3C2;"></i> Datos de los Usuarios
             </h5>
           </div>
           <div class="card-body p-4" style="background-color: #f9f9f9;">
             <table id="tabla-personal" class="table table-striped table-hover table-bordered">
-              <thead style="background-color: #caf0f8; color: #003366;">
+              <thead style="background-color: #001F3F; color: #003366;">
                 <tr>
-                  <th><i class="fas fa-hashtag"></i></th>
-                  <th><i class="fas fa-id-card"></i> Tipo Documento</th>
-                  <th><i class="fas fa-address-card"></i> Nro. Documento</th>
-                  <th><i class="fas fa-user"></i> Apellidos</th>
-                  <th><i class="fas fa-user"></i> Nombres</th>
-                  <th><i class="fas fa-map-marker-alt"></i> Dirección</th>
-                  <th><i class="fas fa-user-circle"></i> Cuenta Usuario</th>
-                  <th><i class="fas fa-envelope"></i> Usuario</th> <!-- Columna de correo -->
-                  <th><i class="fas fa-sync-alt"></i> Estado</th>
+                  <th style="background-color: #001F3F; color: #EFE3C2"><i class="fas fa-hashtag"></i></th>
+                  <th style="background-color: #001F3F; color: #EFE3C2"><i class="fas fa-id-card"></i> Tipo Documento</th>
+                  <th style="background-color: #001F3F; color: #EFE3C2"><i class="fas fa-address-card"></i> Nro. Documento</th>
+                  <th style="background-color: #001F3F; color: #EFE3C2"><i class="fas fa-user"></i> Apellidos</th>
+                  <th style="background-color: #001F3F; color: #EFE3C2"><i class="fas fa-user"></i> Nombres</th>
+                  <th style="background-color: #001F3F; color: #EFE3C2"><i class="fas fa-map-marker-alt"></i> Dirección</th>
+                  <th style="background-color: #001F3F; color: #EFE3C2"><i class="fas fa-user-circle"></i> Cuenta Usuario</th>
+                  <th style="background-color: #001F3F; color: #EFE3C2"><i class="fas fa-envelope"></i> Usuario</th> <!-- Columna de correo -->
+                  <th style="background-color: #001F3F; color: #EFE3C2"><i class="fas fa-sync-alt"></i> Estado</th>
                   <th style="display:none;">ID Usuario</th> <!-- Columna oculta -->
                 </tr>
               </thead>
@@ -121,6 +118,7 @@
               </tbody>
             </table>
           </div>
+
         </div>
       </div>
     </div>
