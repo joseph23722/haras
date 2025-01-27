1fd8345e695e1c310399267f497c6bcc88616eb1
Francescoli <vicharramarcosgereda@gmail.com>
2025-01-27 18:28:06 -0500

    vista medicamentos usados

--- views/servicios/listar-medicamento-usado.php
+++ views/servicios/listar-medicamento-usado.php
@@ -1,12 +1,7 @@
 <?php require_once '../header.php'; ?>
 
 <div class="container-fluid px-4">
-    <h1 class="mt-4 text-center text-uppercase" style="font-weight: bold; font-size: 32px; color: #005b99;">Medicamentos Aplicados</h1>
-    <ol class="breadcrumb mb-4 p-2 rounded" style="background-color: #e8f1f8;">
-        <li class="breadcrumb-item active" style="color: #004085; font-weight: bold;">
-            <i class="fas fa-info-circle" style="color: #007bff;"></i> Listado de medicamentos aplicados
-        </li>
-    </ol>
+    <h1 class="mt-4 text-center text-uppercase" style="font-weight: bold; font-size: 32px; color: #000;">Medicamentos Aplicados</h1>
 
     <div class="card mb-4 shadow-sm border-0">
         <div class="card-body">
